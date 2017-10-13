7e00673ab3ca23e8db751d1e0b5053dabcdabb3b
Nguyen Dinh Hieu <dinhhieu16598@gmail.com>
2017-10-13 20:55:49 +0700

    Chapter 11

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009134804) do
+ActiveRecord::Schema.define(version: 20171013120832) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20171009134804) do
     t.string "gender"
     t.string "address"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
